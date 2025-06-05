1e5e69fa6dd6baab25242ba2faae6be85d2113cf
fadillah <153173946+MuhammadFadillah09@users.noreply.github.com>
2025-06-05 19:25:04 +0700

    Add files via upload

---
+++ TugasBab3-main/TugasBab3-main/src/data.jsx
@@ -0,0 +1,137 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Popular',
+    },
+    {
+      id: 2,
+      categoryName: 'Wisata',
+    },
+    {
+      id: 3,
+      categoryName: 'Kuliner',
+    },
+    {
+      id: 4,
+      categoryName: 'Budaya',
+    },
+    {
+      id: 5,
+      categoryName: 'IKN',
+    },
+    {
+      id: 6,
+      categoryName: 'Lifestyle',
+    },
+  ];
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Waterboom Pesona Modern Banjar: Wahana, Harga Tiket, Lokasi',
+      category: 'Wisata',
+      image:
+        'https://akcdn.detik.net.id/community/media/visual/2025/05/23/waterboom-pesona-modern-banjar-1748009655199_169.jpeg?w=700&q=90',
+      createdAt: 'Mei 20, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 2,
+      title: 'Waterboom Pesona Modern Banjarmasin: Jam Buka dan Harga Tiket Terbaru',
+      category: 'Wisata',
+      image:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,f_auto,q_auto:best,w_1024/v1634025439/01j22p3cgr2kena4j2tsc98tsr.jpg',
+      createdAt: 'Mei 10, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 3,
+      title: 'Wisata Alam di Banjarmasin yang Legend dan Hits, Cocok untuk Liburan Keluarga',
+      category: 'Wisata',
+      image:
+        'https://assets.pikiran-rakyat.com/crop/0x0:0x0/703x0/webp/photo/2025/04/18/434794870.jpg',
+      createdAt: 'Mei 01, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 4,
+      title: 'Keren! Jadi Tahu Tempat Wisata di Banjarmasin',
+      category: 'Wisata',
+      image:
+        'https://thumb.viva.id/vivabanyuwangi/1265x711/2025/05/10/681ec228a4426-taman-siring-destinasi-wisata-populer-di-samarinda_banyuwangi.jpg',
+      createdAt: 'Mar 22, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 5,
+      title: 'DI SINI IKAN BAKARNYA ENAK-ENAK | Rumah Makan Depot Sari Patin Banjarmasin',
+      category: 'Kuliner',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbUkYydYXhcB_tqzAWqOZNlarv1A77EpdA3g&s',
+      createdAt: 'Mar 20, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 6,
+      title: ' Pesona Meratus, Pegunungan Batu Tua Pulau Kalimantan',
+      category: 'Wisata',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkBKOchk7M-7FzlTWHjC0toOKjftyFr3lieQ&s',
+      createdAt: 'Mar 15, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 7,
+      title: 'Eksotika Objek Wisata Alam Tanah Bumbu Pantai Angsana',
+      category: 'Wisata',
+      image:
+        'https://img.okezone.com/content/2017/06/27/406/1725349/share-loc-pantai-angsana-kalimantan-selatan-jadi-buruan-wisatawan-di-libur-lebaran-DnHp8aV18W.jpg',
+      createdAt: 'Mar 05, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 8,
+      title: 'Objek Wisata Danau Seran Banjarbaru',
+      category: 'Wisata',
+      image:
+        'https://mediacenter.banjarbarukota.go.id/wp-content/uploads/2022/05/WhatsApp-Image-2022-05-19-at-15.51.00-1-1.jpeg',
+      createdAt: 'Feb 25, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 9,
+      title: 'Mencicipi Segarnya Soto Banjar Bang Amat di Banjarmasin',
+      category: 'Kuliner',
+      image:
+        'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,f_auto,q_auto:best,w_640/v1566732416/nwf0vak1p1vsezegi5g7.jpg',
+      createdAt: 'Feb 19, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 10,
+      title: 'Taman Siring 0 KM Banjarmasin, Ikon Wisata Tepi Sungai Martapura',
+      category: 'Wisata',
+      image:
+        'https://nabilrental.com/wp-content/uploads/2025/03/taman-siring.webp',
+      createdAt: 'Feb 12, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 11,
+      title: 'Keindahan Pasar Terapung Siring Tendean Banjarmasin',
+      category: 'Wisata',
+      image:
+        'https://cdn.wisata.app/diary/11501120-5039-80f4-bcfe-f2f42aa91175_sm.jpg',
+      createdAt: 'Feb 03, 2025',
+      totalComments: 89,
+    },
+    {
+      id: 12,
+      title: 'Menara Pandang: Menjelajahi Pesona Kota Seribu Sungai dari Atas Awan',
+      category: 'Wisata',
+      image:
+        'https://indonesiakaya.com/wp-content/uploads/2023/05/Menara-Pandang-Banjarmasin-5.jpeg',
+      createdAt: 'Jan 25, 2025',
+      totalComments: 89,
+    },
+  ];
+  
