87fc799a4fa734e520eaece8848b8b78b7ef24f1
fadillah <153173946+MuhammadFadillah09@users.noreply.github.com>
2025-06-05 19:30:44 +0700

    Add files via upload

---
+++ TugasBab4-main/TugasBab4-main/src/data.jsx
@@ -0,0 +1,171 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Popular',
+    },
+    {
+      id: 2,
+      categoryName: 'Wisata',
+    },
+    {
+      id: 3,
+      categoryName: 'Kuliner',
+    },
+    {
+      id: 4,
+      categoryName: 'Budaya',
+    },
+    {
+      id: 5,
+      categoryName: 'IKN',
+    },
+    {
+      id: 6,
+      categoryName: 'Lifestyle',
+    },
+  ];
+  
+  export const BlogList = [
+  {
+    id: 1,
+    title: 'Waterboom Pesona Modern Banjar: Wahana, Harga Tiket, Lokasi',
+    category: 'Wisata',
+    image:
+      'https://akcdn.detik.net.id/community/media/visual/2025/05/23/waterboom-pesona-modern-banjar-1748009655199_169.jpeg?w=700&q=90',
+    createdAt: 'Nov 10, 2025',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 2,
+    title: 'Waterboom Pesona Modern Banjarmasin: Jam Buka dan Harga Tiket Terbaru',
+    category: 'Wisata',
+    image:
+      'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,f_auto,q_auto:best,w_1024/v1634025439/01j22p3cgr2kena4j2tsc98tsr.jpg',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 3,
+    title: '5 Wisata Alam di Banjarmasin yang Legend dan Hits, Cocok untuk Liburan Keluarga',
+    category: 'Car',
+    image:
+      'https://assets.pikiran-rakyat.com/crop/0x0:0x0/703x0/webp/photo/2025/04/18/434794870.jpg',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 4,
+    title: 'Keren! Jadi Tahu Tempat Wisata di Banjarmasin',
+    category: 'Food',
+    image:
+      'https://thumb.viva.id/vivabanyuwangi/1265x711/2025/05/10/681ec228a4426-taman-siring-destinasi-wisata-populer-di-samarinda_banyuwangi.jpg',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 5,
+    title: 'Menara Pandang: Menjelajahi Pesona Kota Seribu Sungai dari Atas Awan',
+    category: 'Music',
+    image:
+      'https://indonesiakaya.com/wp-content/uploads/2023/05/Menara-Pandang-Banjarmasin-5.jpeg',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: `In recent years, there has been a remarkable resurgence in the popularity of vinyl records, leading to a renewed interest in analog music experiences. The unmistakable crackle as the needle meets the groove, the tangible connection to album artwork, and the intentional act of flipping a record—all these elements contribute to the unique charm of vinyl. This rekindled fascination with analog isn't just a fleeting trend; it reflects a genuine desire for a more immersive and authentic music encounter in an age dominated by digital convenience. As new generations embrace the vintage allure of vinyl, a rich appreciation for the past intertwines with modern sensibilities, making the act of rediscovering vinyl a bridge between nostalgia and contemporary culture.`,
+  },
+  {
+    id: 6,
+    title: 'Keindahan Pasar Terapung Siring Tendean Banjarmasin',
+    category: 'Wisata',
+    image:
+      'https://cdn.wisata.app/diary/11501120-5039-80f4-bcfe-f2f42aa91175_sm.jpg',
+    createdAt: 'Jul 25, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 7,
+    title: 'Taman Siring 0 KM Banjarmasin, Ikon Wisata Tepi Sungai Martapura',
+    category: 'Wisata',
+    image:
+      'https://nabilrental.com/wp-content/uploads/2025/03/taman-siring.webp',
+    createdAt: 'Jul 25, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 8,
+    title: 'Mencicipi Segarnya Soto Banjar Bang Amat di Banjarmasin',
+    category: 'Kuliner',
+    image:
+      'https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,f_auto,q_auto:best,w_640/v1566732416/nwf0vak1p1vsezegi5g7.jpg',
+    createdAt: 'Jul 25, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 9,
+    title: 'IKAN BAKARNYA ENAK-ENAK | Rumah Makan Depot Sari Patin Banjarmasin',
+    category: 'Kuliner',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbUkYydYXhcB_tqzAWqOZNlarv1A77EpdA3g&s',
+    createdAt: 'Jul 25, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 10,
+    title: 'Objek Wisata Danau Seran Banjarbaru',
+    category: 'Wisata',
+    image:
+      'https://mediacenter.banjarbarukota.go.id/wp-content/uploads/2022/05/WhatsApp-Image-2022-05-19-at-15.51.00-1-1.jpeg',
+    createdAt: 'Jul 25, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 11,
+    title: 'Eksotika Objek Wisata Alam Tanah Bumbu Pantai Angsana',
+    category: 'Wisata',
+    image:
+      'https://ruko.s3.ap-southeast-1.amazonaws.com/rumahkomunitas.com/tulisan/280220044130_senja-pantai-angsana-1-580x408.jpg',
+    createdAt: 'Jul 25, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+  {
+    id: 12,
+    title: 'Pesona Meratus, Pegunungan Batu Tua Pulau Kalimantan',
+    category: 'Wisata',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkBKOchk7M-7FzlTWHjC0toOKjftyFr3lieQ&s',
+    createdAt: 'Jul 25, 2023',
+    totalComments: 89,
+    totalLikes:2300,
+    content: ``,
+  },
+];
+
+export const ProfileData = {
+  profilePict:
+    'https://awsimages.detik.net.id/community/media/visual/2018/03/03/39f24229-6f26-4a17-aa92-44c3bd3dae9e_43.jpeg?w=600&q=90',
+  name: 'Muhammad Fadillah',
+  createdAt: '18 Mar, 2020',
+  blogPosted: 29,
+  following: 31000,
+  follower: 528,
+};
